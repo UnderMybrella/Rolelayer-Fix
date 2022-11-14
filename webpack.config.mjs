51ea9c3dd681315cb3cbe1b3a7a48aad07a4c0fc
Izzy <undermybrella@abimon.org>
2022-11-14 14:02:09 +1100

    Reorganise to work with Firefox and Chrome

--- webpack.config.mjs
+++ webpack.config.mjs
@@ -1,22 +1,17 @@
 import path from 'path';
 import WebExtPlugin from 'web-ext-plugin';
-import { fileURLToPath } from 'url';
+import CopyPlugin from 'copy-webpack-plugin';
+import {fileURLToPath} from 'url';
 
 const __filename = fileURLToPath(import.meta.url);
 
 const __dirname = path.dirname(__filename);
 
-// noinspection JSUnusedGlobalSymbols
-export default {
+const CommonConfig = {
     entry: {
-        background: "./src/background.ts",
+        service: "./src/service.ts",
         format: "./src/format.ts",
         test: "./src/test.ts"
-    },
-    output: {
-        path: path.resolve(__dirname, "addon/dist"),
-        // filename: "[name].js",
-        clean: true
     },
     mode: 'none',
     module: {
@@ -25,11 +20,54 @@ export default {
                 test: /\.tsx?$/,
                 use: 'ts-loader',
                 exclude: /node_modules/,
-            },
+            }
         ],
     },
     resolve: {
         extensions: ['.tsx', '.ts', '.js'],
     },
-    plugins: [new WebExtPlugin({ sourceDir: path.resolve(__dirname, "addon") })],
+    plugins: [
+        new CopyPlugin({
+            patterns: [
+                {from: "assets", to: "../assets"}
+            ],
+        }),
+    ],
 };
+
+const FirefoxConfig = {
+    ...CommonConfig,
+    name: 'firefox',
+    output: {
+        path: path.resolve(__dirname, "firefox/dist"),
+        // filename: "[name].js",
+        clean: true
+    },
+    plugins: [
+        ...CommonConfig.plugins,
+        new WebExtPlugin({
+            sourceDir: path.resolve(__dirname, "firefox"),
+            target: 'firefox-desktop'
+        })
+    ],
+};
+
+const ChromeConfig = {
+    ...CommonConfig,
+    name: 'chrome',
+    output: {
+        path: path.resolve(__dirname, "chrome/dist"),
+        // filename: "[name].js",
+        clean: true
+    },
+    plugins: [
+        ...CommonConfig.plugins,
+        new WebExtPlugin({
+            sourceDir: path.resolve(__dirname, "chrome"),
+            target: 'chromium'
+        })
+    ],
+}
+
+// noinspection JSUnusedGlobalSymbols
+export default [FirefoxConfig, ChromeConfig];
